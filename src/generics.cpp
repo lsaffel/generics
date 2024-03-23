758e6be990081142338f3a17a938fa59489bc652
Lisa <llvvss@icloud.com>
2024-03-23 12:20:59 -0600

    added generic template

--- src/generics.cpp
+++ src/generics.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 using namespace std;
-template<typename T>
 
+template<typename T>
 void Swap(T& a, T& b) {
-    T temp = a;
+    T temp = a;        // T here as the type, instead of int or char
     a = b;
     b = temp;
 }
 
+// an alternate way to write this template, putting Type 
+// (or whatever you prefer) instead of T,
+// although T is common in the literature:
+// template<typename >
+// void Swap(Type& a, Type& b) {
+//     Type temp = a;        // T here as the type, instead of int or char
+//     a = b;
+//     b = temp;
+// }
 
 int main()
 {
     int a = 5, b = 7;
     cout << a << " - " << b << endl;
-    Swap(a , b);
+    Swap<int>(a , b);        // this also works with Swap(a, b). 
+                        // Putting <int> is not required here
     cout << a << " - " << b << endl;
 
     char c = 'c', d = 'd';
     cout << c << " - " << d << endl;
 
-    Swap(c, d);
+    Swap<char>(c, d);       // <char> is not necessary here either.
+            // the type happens atuomatically if you just put:
+            // Swap(c, d);
     cout << c << " - " << d << endl;
 
 }
